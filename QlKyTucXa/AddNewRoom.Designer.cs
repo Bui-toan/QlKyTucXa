108a652eda62d14d466f4d406adb4a73b7673811
buitoan123 <khanhtoanx19@gmail.com>
2024-11-04 23:58:15 +0700

    Design

---
+++ QlKyTucXa/AddNewRoom.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace QlKyTucXa
+{
+	partial class AddNewRoom
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.guna2RadioButton1 = new Guna.UI2.WinForms.Guna2RadioButton();
+			this.guna2RadioButton2 = new Guna.UI2.WinForms.Guna2RadioButton();
+			this.guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
+			this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).BeginInit();
+			this.guna2GroupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// guna2RadioButton1
+			// 
+			this.guna2RadioButton1.AutoSize = true;
+			this.guna2RadioButton1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2RadioButton1.CheckedState.BorderThickness = 0;
+			this.guna2RadioButton1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2RadioButton1.CheckedState.InnerColor = System.Drawing.Color.White;
+			this.guna2RadioButton1.CheckedState.InnerOffset = -4;
+			this.guna2RadioButton1.Location = new System.Drawing.Point(249, 61);
+			this.guna2RadioButton1.Name = "guna2RadioButton1";
+			this.guna2RadioButton1.Size = new System.Drawing.Size(62, 24);
+			this.guna2RadioButton1.TabIndex = 0;
+			this.guna2RadioButton1.Text = "Nam";
+			this.guna2RadioButton1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+			this.guna2RadioButton1.UncheckedState.BorderThickness = 2;
+			this.guna2RadioButton1.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+			this.guna2RadioButton1.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+			// 
+			// guna2RadioButton2
+			// 
+			this.guna2RadioButton2.AutoSize = true;
+			this.guna2RadioButton2.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2RadioButton2.CheckedState.BorderThickness = 0;
+			this.guna2RadioButton2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2RadioButton2.CheckedState.InnerColor = System.Drawing.Color.White;
+			this.guna2RadioButton2.CheckedState.InnerOffset = -4;
+			this.guna2RadioButton2.Location = new System.Drawing.Point(351, 61);
+			this.guna2RadioButton2.Name = "guna2RadioButton2";
+			this.guna2RadioButton2.Size = new System.Drawing.Size(50, 24);
+			this.guna2RadioButton2.TabIndex = 1;
+			this.guna2RadioButton2.Text = "Nữ";
+			this.guna2RadioButton2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+			this.guna2RadioButton2.UncheckedState.BorderThickness = 2;
+			this.guna2RadioButton2.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+			this.guna2RadioButton2.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+			// 
+			// guna2DataGridView1
+			// 
+			dataGridViewCellStyle19.BackColor = System.Drawing.Color.White;
+			this.guna2DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle19;
+			this.guna2DataGridView1.BackgroundColor = System.Drawing.Color.DimGray;
+			dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle20.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+			dataGridViewCellStyle20.Font = new System.Drawing.Font("Segoe UI", 9F);
+			dataGridViewCellStyle20.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle20.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle20.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle20.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.guna2DataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle20;
+			this.guna2DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridViewCellStyle21.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle21.BackColor = System.Drawing.Color.White;
+			dataGridViewCellStyle21.Font = new System.Drawing.Font("Segoe UI", 9F);
+			dataGridViewCellStyle21.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+			dataGridViewCellStyle21.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+			dataGridViewCellStyle21.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+			dataGridViewCellStyle21.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle21;
+			this.guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+			this.guna2DataGridView1.Location = new System.Drawing.Point(13, 101);
+			this.guna2DataGridView1.Name = "guna2DataGridView1";
+			this.guna2DataGridView1.RowHeadersVisible = false;
+			this.guna2DataGridView1.RowHeadersWidth = 51;
+			this.guna2DataGridView1.RowTemplate.Height = 24;
+			this.guna2DataGridView1.Size = new System.Drawing.Size(457, 146);
+			this.guna2DataGridView1.TabIndex = 2;
+			this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+			this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+			this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+			this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+			this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+			this.guna2DataGridView1.ThemeStyle.BackColor = System.Drawing.Color.DimGray;
+			this.guna2DataGridView1.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.guna2DataGridView1.ThemeStyle.HeaderStyle.Height = 4;
+			this.guna2DataGridView1.ThemeStyle.ReadOnly = false;
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.Height = 24;
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+			this.guna2DataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+			// 
+			// guna2GroupBox1
+			// 
+			this.guna2GroupBox1.Controls.Add(this.guna2DataGridView1);
+			this.guna2GroupBox1.Controls.Add(this.guna2RadioButton1);
+			this.guna2GroupBox1.Controls.Add(this.guna2RadioButton2);
+			this.guna2GroupBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.guna2GroupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+			this.guna2GroupBox1.Location = new System.Drawing.Point(32, 12);
+			this.guna2GroupBox1.Name = "guna2GroupBox1";
+			this.guna2GroupBox1.Size = new System.Drawing.Size(737, 298);
+			this.guna2GroupBox1.TabIndex = 3;
+			this.guna2GroupBox1.Text = "guna2GroupBox1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 342);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 16);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Số phòng";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(143, 342);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(112, 22);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(312, 337);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 26);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Tìm kiếm";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(29, 400);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(82, 16);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Số sinh viên ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(190, 425);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 16);
+			this.label3.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(309, 403);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(50, 16);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Giường";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(309, 442);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(31, 16);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Bàn";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(583, 406);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(60, 16);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Điều hòa";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(583, 450);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(44, 16);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "label7";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(143, 400);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(112, 22);
+			this.textBox2.TabIndex = 13;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(143, 444);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(112, 22);
+			this.textBox3.TabIndex = 14;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(420, 403);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(112, 22);
+			this.textBox4.TabIndex = 15;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(420, 444);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(112, 22);
+			this.textBox5.TabIndex = 16;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(657, 400);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(112, 22);
+			this.textBox6.TabIndex = 17;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(657, 450);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(112, 22);
+			this.textBox7.TabIndex = 18;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(29, 442);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(55, 16);
+			this.label8.TabIndex = 19;
+			this.label8.Text = "Tên khu";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(202, 478);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(66, 23);
+			this.button2.TabIndex = 20;
+			this.button2.Text = "Thêm";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(336, 477);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(86, 25);
+			this.button3.TabIndex = 21;
+			this.button3.Text = "Sửa";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(657, 478);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(83, 32);
+			this.button4.TabIndex = 22;
+			this.button4.Text = "Xóa";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(493, 478);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(96, 22);
+			this.button5.TabIndex = 23;
+			this.button5.Text = "Lưu";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// AddNewRoom
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 522);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.guna2GroupBox1);
+			this.Name = "AddNewRoom";
+			this.Text = "AddNewRoom";
+			((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).EndInit();
+			this.guna2GroupBox1.ResumeLayout(false);
+			this.guna2GroupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Guna.UI2.WinForms.Guna2RadioButton guna2RadioButton1;
+		private Guna.UI2.WinForms.Guna2RadioButton guna2RadioButton2;
+		private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
+		private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+	}
+}
---
+++ QlKyTucXa/Dashboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QlKyTucXa
+{
+	public partial class Dashboard : Form
+	{
+		public Dashboard()
+		{
+			InitializeComponent();
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
